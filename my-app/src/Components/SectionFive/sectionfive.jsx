b8f51f104c41a6322c7a6fe22150cf7c9ee0c73d
DevMayani <106169881+DevMayani@users.noreply.github.com>
2023-11-24 15:26:36 +0100

    ADD NEW

--- my-app/src/Components/SectionFive/sectionfive.jsx
+++ my-app/src/Components/SectionFive/sectionfive.jsx
@@ -17,6 +17,15 @@ const SectionFive = () => {
     const [isDropResult, setDropResult] = useState(false);
 
     const [isDropOnlineVisible, setDropOnlineVisible] = useState(false);
+    const [isDropWorkshop, setDropWorkshop] = useState(false);
+
+    const [isDropBook, setDropBook] = useState(false);
+    const [isDropSchedule, setDropSchedule] = useState(false);
+
+    const [isDropSuit, setDropSuit] = useState(false);
+    const [isDropRemote, setDropRemote] = useState(false);
+
+    const [isDropProgram, setDropProgram] = useState(false);
 
 
 
@@ -40,7 +49,24 @@ const SectionFive = () => {
     const toggleOnlineVisible = () => {
         setDropOnlineVisible(!isDropOnlineVisible)
     }
-
+    const toggleWorkshop = () => {
+      setDropWorkshop(!isDropWorkshop)
+  }
+  const toggleBook = () => {
+    setDropBook(!isDropBook)
+}
+const toggleSchedule = () => {
+  setDropSchedule(!isDropSchedule)
+}
+const toggleSuit = () => {
+  setDropSuit(!isDropSuit)
+}
+const toggleRemote = () => {
+  setDropRemote(!isDropRemote)
+}
+const toggleProgram = () => {
+  setDropProgram(!isDropProgram)
+}
 
 
     return(
@@ -51,7 +77,7 @@ const SectionFive = () => {
             <div  className={styles.subcont}>
                 <div className={styles.cont} >
                    
-                    <p className={styles.label}>How do I choose the right therapist for me?</p>
+                    <p className={styles.label}>What if I want more therapy sessions than the one my organization booked for me?</p>
                    
                     <IoIosArrowDown
                     onClick={toggleDropdown}
@@ -61,21 +87,21 @@ const SectionFive = () => {
                 </div>
                 {isDropdownVisible && (
         <p className={styles.text}>
-          We understand the importance of finding the right fit. Our platform allows you to share your story which enables our algorithms to assign you to find a therapist who aligns with your needs and preferences.
+         With Theragist, you are able to book extra sessions independent of your organization .
         </p>
       )}
             </div>
 
             <div className={styles.subcont}>
                 <div className={styles.cont} >
-                <p className={styles.label}>Is my information kept confidential?</p>
+                <p className={styles.label}>What is the mode of communication?</p>
                 <IoIosArrowDown
                 onClick={toggleInfoDropVisible}
                 className={`${styles.icon} ${isDropInfoVisible ? styles.icon_active  : ''}`}/>
                 </div>
                 {isDropInfoVisible && (
         <p className={styles.text}>
-           We understand the importance of finding the right fit. Our platform allows you to share your story which enables our algorithms to assign you to find a therapist who aligns with your needs and preferences.
+           We have provisions for video calls, audio calls and chats. You get to decide which you want 
         </p>
       )}
                 
@@ -83,14 +109,15 @@ const SectionFive = () => {
 
             <div className={styles.subcont}>
                 <div className={styles.cont} >
-                <p className={styles.label}>What if I cannot afford therapy?</p>
+                <p className={styles.label}>What can I expect from my first appointment?</p>
                 <IoIosArrowDown
                 onClick={toggleTheraphyVisible}
                 className={`${styles.icon} ${isDropTheraphyVisible ? styles.icon_active  : ''}`}/>
                 </div>
                 {isDropTheraphyVisible && (
         <p className={styles.text}>
-           We understand the importance of finding the right fit. Our platform allows you to share your story which enables our algorithms to assign you to find a therapist who aligns with your needs and preferences.
+          In the first appointment, you will undergo a general and specific diagnosis which will dictate the type of therapy/counseling you'll receive for the remaining appointments. You'll then set initial treatment goals with your Counsellor/Therapist. 
+
         </p>
       )}
                 
@@ -99,28 +126,102 @@ const SectionFive = () => {
 
              <div className={styles.subcont}>
                 <div className={styles.cont} >
-                <p className={styles.label}>How long will it take to see results in therapy?</p>
+                <p className={styles.label}>Is my information kept confidential?</p>
                 <IoIosArrowDown
                 onClick={toggleResultVisible}
                 className={`${styles.icon} ${isDropResult ? styles.icon_active  : ''}`}/>
                 </div>
                 {isDropResult && (
         <p className={styles.text}>
-         We understand the importance of finding the right fit. Our platform allows you to share your story which enables our algorithms to assign you to find a therapist who aligns with your needs and preferences.
+         Certainly, in fact, you can remain anonymous if you want to. 
         </p>
       )}
                 
             </div>
             <div className={styles.subcont}>
                 <div className={styles.cont} >
-                <p className={styles.label}>Is online therapy as effective as in-person therapy?</p>
+                <p className={styles.label}>Can I book a session on behalf of someone else?</p>
                 <IoIosArrowDown
-                onClick={toggleOnlineVisible}
-                className={`${styles.icon} ${isDropOnlineVisible ? styles.icon_active  : ''}`}/>
+                onClick={toggleBook}
+                className={`${styles.icon} ${isDropBook ? styles.icon_active  : ''}`}/>
                 </div>
-                {isDropOnlineVisible && (
+                {isDropBook && (
         <p className={styles.text}>
-           We understand the importance of finding the right fit. Our platform allows you to share your story which enables our algorithms to assign you to find a therapist who aligns with your needs and preferences.
+           Yes, you can, without consent if they are minors and are your wards or children, but if they are adults, you must have their consent.
+        </p>
+      )}
+                
+            </div>
+
+            <div className={styles.subcont}>
+                <div className={styles.cont} >
+                <p className={styles.label}> Who will be facilitating the workshop?</p>
+                <IoIosArrowDown
+                onClick={toggleWorkshop}
+                className={`${styles.icon} ${isDropWorkshop ? styles.icon_active  : ''}`}/>
+                </div>
+                {isDropWorkshop && (
+        <p className={styles.text}>
+           The workshop will be facilitated by seasoned mental health professionals from Theragist. Our team brings expertise in stress management and corporate well-being, ensuring a valuable and impactful experience for participants.
+
+        </p>
+      )}
+                
+            </div>
+            <div className={styles.subcont}>
+                <div className={styles.cont} >
+                <p className={styles.label}> How can my organization schedule a workshop?</p>
+                <IoIosArrowDown
+                onClick={toggleSchedule}
+                className={`${styles.icon} ${isDropSchedule ? styles.icon_active  : ''}`}/>
+                </div>
+                {isDropSchedule && (
+        <p className={styles.text}>
+           Scheduling a workshop is easy! Simply contact us through the provided channels on our website, and our team will assist you in choosing a suitable date and format for the Program.
+        </p>
+      )}
+                
+            </div>
+            <div className={styles.subcont}>
+                <div className={styles.cont} >
+                <p className={styles.label}>Can the workshop be customized to suit the specific needs of our organization?</p>
+                <IoIosArrowDown
+                onClick={toggleSuit}
+                className={`${styles.icon} ${isDropSuit ? styles.icon_active  : ''}`}/>
+                </div>
+                {isDropSuit && (
+        <p className={styles.text}>
+           Absolutely! We understand that each organization is unique. We offer customization options to tailor the workshop content to address the specific challenges and goals of your team.
+
+        </p>
+      )}
+                
+            </div>
+            <div className={styles.subcont}>
+                <div className={styles.cont} >
+                <p className={styles.label}>Are  the workshops suitable for remote teams?</p>
+                <IoIosArrowDown
+                onClick={toggleRemote}
+                className={`${styles.icon} ${isDropRemote ? styles.icon_active  : ''}`}/>
+                </div>
+                {isDropRemote && (
+        <p className={styles.text}>
+           Yes, the virtual session option is ideal for remote teams. It provides the same transformative experience, allowing participants to engage in the workshop from the comfort of their own workspace.
+        </p>
+      )}
+                
+            </div>
+            <div className={styles.subcont}>
+                <div className={styles.cont} >
+                <p className={styles.label}>How can we measure the impact of mental wellness programs on our organization?</p>
+                <IoIosArrowDown
+                onClick={toggleProgram}
+                className={`${styles.icon} ${isDropProgram ? styles.icon_active  : ''}`}/>
+                </div>
+                {isDropProgram && (
+        <p className={styles.text}>
+           We provide post-workshop evaluations and assessments to measure the impact on participants. Additionally, our team is available for follow-up consultations and can assist in analyzing the positive changes within your organization post-program.
+
         </p>
       )}
                 
