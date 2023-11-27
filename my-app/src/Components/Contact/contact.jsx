bace4f4be67342466bab4885a1ffbc4a918fa428
DevMayani <106169881+DevMayani@users.noreply.github.com>
2023-11-27 10:14:05 +0100

    new

--- my-app/src/Components/Contact/contact.jsx
+++ my-app/src/Components/Contact/contact.jsx
@@ -1,6 +1,7 @@
 // ContactInfo.js
 import React from 'react';
 import { FaInstagram } from 'react-icons/fa';
+import Link from 'next/link';
 import styles from './contact.module.css'
 
 const ContactInfo = () => {
@@ -19,6 +20,18 @@ const ContactInfo = () => {
       <p className={styles.texts}>
        <a className={styles.link} href="mailto:info@theragist.com">info@theragist.com</a>
       </p>
+        </div>
+        <div className={styles.cont}>
+        <p className={styles.text}>
+       Phone:
+      </p>
+      
+      <p className={styles.texts}>
+      <a href="tel:+2348123831687" className={styles.link}>
+      +234-812-383-1687
+      </a>
+      </p>
+      
         </div>
     
      
